873bd8016ab63c67fecba804ed2df5fe9248df5a
AntonBs <reloded1995@gmail.com>
2021-10-03 15:21:42 +0300

    Admin header,nav,sidebar,footer

---
+++ resources/views/backend/layouts/head.blade.php
@@ -0,0 +1,17 @@
+<title>Admin</title>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+<meta name="description" content="Lucid Bootstrap 4.1.1 Admin Template">
+<meta name="author" content="WrapTheme, design by: ThemeMakker.com">
+
+<link rel="icon" href="{{asset('favicon.ico')}}" type="image/x-icon">
+<!-- VENDOR CSS -->
+<link rel="stylesheet" href="{{asset('backend/vendor/bootstrap/css/bootstrap.min.css')}}">
+<link rel="stylesheet" href="{{asset('backend/vendor/font-awesome/css/font-awesome.min.css')}}">
+<link rel="stylesheet" href="{{asset('backend/vendor/jvectormap/jquery-jvectormap-2.0.3.min.css')}}"/>
+<link rel="stylesheet" href="{{asset('backend/vendor/morrisjs/morris.min.css')}}" />
+
+<!-- MAIN CSS -->
+<link rel="stylesheet" href="{{asset('backend/assets/css/main.css')}}">
+<link rel="stylesheet" href="{{asset('backend/assets/css/color_skins.css')}}">
